76e2ce8162ac2dfe3f62d3df4bc6bd03e4a30005
sriseven07 <85227586+sriseven07@users.noreply.github.com>
2024-04-19 21:35:56 +0530

    navbar changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className="navbar">
+                <div className="heading">
+                    <Link>
+                        <h1>TESBI</h1>
+                    </Link>
+                </div>
+                <div className="navButton">
+                    <button className='navBtn'>Login</button>
+                    <button className='navBtn'>Signup</button>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar;
