f9ff864d4362da569de741d8366fd64708ee1a96
sriseven07 <85227586+sriseven07@users.noreply.github.com>
2024-04-20 01:18:38 +0530

    new sidebar

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,38 @@
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <div className="menu-item">
+                <span class="material-symbols-outlined">sprint</span>
+                <a href='#'>Activity</a>
+            </div>
+            <div className="menu-item">
+                <span class="material-symbols-outlined">store</span>
+                <a href='#'>Store</a>
+            </div>
+            <div className="menu-item">
+                <span class="material-symbols-outlined">shopping_cart</span>
+                <a href='#'>Purchase</a>
+            </div>
+            <div className="menu-item">
+                <span class="material-symbols-outlined">currency_rupee</span>
+                <a href='#'>Accounts</a>
+            </div>
+            <div className="menu-item">
+                <span class="material-symbols-outlined">concierge</span>
+                <a href='#'>Enquiry</a>
+            </div>
+            <div className="menu-item">
+                <span class="material-symbols-outlined">engineering</span>
+                <a href='#'>Project</a>
+            </div>
+            <div className="menu-item">
+                <span class="material-symbols-outlined">receipt_long</span>
+                <a href='#'>Support</a>
+            </div>
+        </div>
+
+    )
+}
+
+export default Menu;
